59cb83de1819f344965af715d31483e702653de1
Emon Karmoker <emonkarmaker101@gmail.com>
2026-02-26 15:01:49 +0600

    added frontend to CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+﻿from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
@@ -38,6 +38,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ai-reservation-crm-system.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
