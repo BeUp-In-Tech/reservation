7a54589eeed165a3f44f0c5a68447dcb7921b726
Emon Karmoker <emonkarmaker101@gmail.com>
2026-04-16 16:20:35 +0600

    emon

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://ai-reservation-crm-system.onrender.com",
-        "https://ai-reservation-crm-system-la86.onrender.com"
+        "https://ai-reservation-crm-system-xjz3.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
